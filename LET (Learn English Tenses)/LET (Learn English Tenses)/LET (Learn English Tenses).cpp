03853ac4d73bb03d37f6ad9dbf3f8870bbd6be10
Nikolay Brankov <NPBrankov19@codingburgas.bg>
2021-02-07 16:55:14 +0200

    Add Present Continuous Tense Exercises And Usage

--- LET (Learn English Tenses)/LET (Learn English Tenses)/LET (Learn English Tenses).cpp
+++ LET (Learn English Tenses)/LET (Learn English Tenses)/LET (Learn English Tenses).cpp
@@ -619,6 +619,477 @@ void practisePresentSimpleTenseEx5(string mutualChoice)
 		points++;
 	}
 }
+void displayTheUsageOfPresentContinuousTense(string mutualChoice)
+{
+	system("cls");
+	printf("\t\t\t\t\t   Welcome to LET (Learn English Tenses)!");
+	printf("\n\n\t\t\t\t\t    The Usage Of Present Continuous Tense.");
+	printf("\n\n\n 1. First, we use the present continuous for things that are happening at the moment of speaking.");
+	printf("\n\n\t- I'm working at the moment.");
+	printf("\n\n\t- Please call back as we are eating dinner now.");
+	printf("\n\n\t- Julie is sleeping.");
+	printf("\n\n\n 2. We can also use this tense for kinds of temporary situations, even if the action isn't happening at this moment.");
+	printf("\n\n\t- John's working in a bar until he finds a job in his field. (He might not be working now.)");
+	printf("\n\n\t- I'm reading a really great book.");
+	printf("\n\n\t- She's staying with her friend for a week.");
+	printf("\n\n\n 3. We can use the present continuous for temporary or new habits.");
+	printf("\n\n\t- He's eating a lot these days.");
+	printf("\n\n\t- She's swimming every morning (she didn't use to do this).");
+	printf("\n\n\t- You're smoking too much.");
+	printf("\t\t\t\t\tPress Enter To Continue: ");
+	getline(cin, mutualChoice);
+}
+void practisePresentContinuousTenseEx1(string mutualChoice)
+{
+	string sentence[10] = { "1. I am/is/are reading a book.","2. You am/is/are playing football.",
+	"3. He am/is/are my sleeping.","4. She am/is/are dancing.","5. It am/is/are eating bamboo.",
+	"6. We am/is/are running.","7. You am/is/are talking too much.","8. My sister am/is/are riding her bike.",
+	"9. The birds am/is/are flying.", "10. She am/is/are playing the piano." };
+	string condition[1] = { "Enter The Correct Answer: " };
+	system("cls");
+	printf("\t\t\t\t\t   Welcome to LET (Learn English Tenses)!");
+	printf("\n\n\nEx.1 Am, Is Or Are.");
+	cout << "\n\n" << sentence[0];
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "am")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[1];
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "are")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[2];
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "is")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[3];
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "is")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[4];
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "is")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[5];
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "are")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[6];
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "are")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[7];
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "is")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[8];
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "are")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[9];
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "is")
+	{
+		points++;
+	}
+}
+void practisePresentContinuousTenseEx2(string mutualChoice)
+{
+	string sentence[10] = { "1. I ___ football.", "2. You ___ tennis.","3. He ___ video games.",
+	"4. She ___ rugby.","5. It ___ golf.", "6. We ___ the guitar.","7. You ___ the piano.",
+	"8. They ___ basketball.","9. She ___ volleyball.", "10. It ___ table tennis." };
+	string answers[1] = { "\n\n   a) am playing      b) is playing\n\n\t    c) are playing" };
+	string condition[1] = { "Enter The Correct Answer (Enter The Letter Only!): " };
+	system("cls");
+	printf("\t\t\t\t\t   Welcome to LET (Learn English Tenses)!");
+	printf("\n\n\nEx.2 Choose The Correct Answer.");
+	cout << "\n\n" << sentence[0];
+	cout << answers[0];
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "a" || mutualChoice == "a)")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[1];
+	cout << answers[0];
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "c" || mutualChoice == "c)")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[2];
+	cout << answers[0];
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "b" || mutualChoice == "b)")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[3];
+	cout << answers[0];
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "b" || mutualChoice == "b)")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[4];
+	cout << answers[0];
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "b" || mutualChoice == "b)")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[5];
+	cout << answers[0];
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "c" || mutualChoice == "c)")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[6];
+	cout << answers[0];
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "c" || mutualChoice == "c)")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[7];
+	cout << answers[0];
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "c" || mutualChoice == "c)")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[8];
+	cout << answers[0];
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "b" || mutualChoice == "b)")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[9];
+	cout << answers[0];
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "b" || mutualChoice == "b)")
+	{
+		points++;
+	}
+}
+void practisePresentContinuousTenseEx3(string mutualChoice)
+{
+	string sentence[10] = { "1. The boys is playing football now.","2. They are learn Spanish now.",
+	"3. Why aren't you do your homework?","4. Suzan are ironing her clothes right now.",
+	"5. Marry is at university. She is study chemistry.","6. She is works at this moment.",
+	"7. They isn't studying English these days.","8. She is constantly changed her hair color.",
+	"9. Children is studying their lessons.","10. The man is talks on the phone now." };
+	string condition[1] = { "Enter The Correct Form Of The Mistake: " };
+	system("cls");
+	printf("\t\t\t\t\t   Welcome to LET (Learn English Tenses)!");
+	printf("\n\n\nEx.3 Correct The Mistakes (Only The Incorrect Word).");
+	cout << "\n\n" << sentence[0];
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "are")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[1];
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "learning")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[2];
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "doing")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[3];
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "is")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[4];
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "studying")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[5];
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "working")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[6];
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "aren't")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[7];
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "changing")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[8];
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "are")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[9];
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "talking")
+	{
+		points++;
+	}
+}
+void practisePresentContinuousTenseEx4(string mutualChoice)
+{
+	string incorrectWord;
+	string sentence[10] = { "1. Look! Tom is ridings his bike.","2. Alec ist doing a test right now.",
+	"3. The boys ares running in the yard at the moment.","4. I am watchinges very interesting film.",
+	"5. We areing listening to the classical music now.","6. He is nots learning German right now.",
+	"7. Is Nick drinkings coffer at this moment.","8. Are theyr playing football at this moment?",
+	"9. Arent you singing a new song right now?","10. We aret not helping Andrew at this moment." };
+	string condition[2] = { "Enter The Incorrect Word: ", "Enter The Unnecessary part: " };
+	system("cls");
+	printf("\t\t\t\t\t   Welcome to LET (Learn English Tenses)!");
+	printf("\n\n\nEx.4 Erase The Unnecessary Part.");
+	cout << "\n\n" << sentence[0];
+	cout << "\n\n" << condition[0];
+	getline(cin, incorrectWord);
+	cout << "\n\n" << condition[1];
+	getline(cin, mutualChoice);
+	if (incorrectWord == "ridings" && mutualChoice == "s")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[1];
+	cout << "\n\n" << condition[0];
+	getline(cin, incorrectWord);
+	cout << "\n\n" << condition[1];
+	getline(cin, mutualChoice);
+	if (incorrectWord == "ist" && mutualChoice == "t")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[2];
+	cout << "\n\n" << condition[0];
+	getline(cin, incorrectWord);
+	cout << "\n\n" << condition[1];
+	getline(cin, mutualChoice);
+	if (incorrectWord == "ares" && mutualChoice == "s")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[3];
+	cout << "\n\n" << condition[0];
+	getline(cin, incorrectWord);
+	cout << "\n\n" << condition[1];
+	getline(cin, mutualChoice);
+	if (incorrectWord == "watchinges" && mutualChoice == "es")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[4];
+	cout << "\n\n" << condition[0];
+	getline(cin, incorrectWord);
+	cout << "\n\n" << condition[1];
+	getline(cin, mutualChoice);
+	if (incorrectWord == "areing" && mutualChoice == "ing")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[5];
+	cout << "\n\n" << condition[0];
+	getline(cin, incorrectWord);
+	cout << "\n\n" << condition[1];
+	getline(cin, mutualChoice);
+	if (incorrectWord == "nots" && mutualChoice == "s")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[6];
+	cout << "\n\n" << condition[0];
+	getline(cin, incorrectWord);
+	cout << "\n\n" << condition[1];
+	getline(cin, mutualChoice);
+	if (incorrectWord == "drinkings" && mutualChoice == "s")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[7];
+	cout << "\n\n" << condition[0];
+	getline(cin, incorrectWord);
+	cout << "\n\n" << condition[1];
+	getline(cin, mutualChoice);
+	if (incorrectWord == "theyr" && mutualChoice == "r")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[8];
+	cout << "\n\n" << condition[0];
+	getline(cin, incorrectWord);
+	cout << "\n\n" << condition[1];
+	getline(cin, mutualChoice);
+	if ((incorrectWord == "arent" || incorrectWord == " Arent") && mutualChoice == "nt")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[9];
+	cout << "\n\n" << condition[0];
+	getline(cin, incorrectWord);
+	cout << "\n\n" << condition[1];
+	getline(cin, mutualChoice);
+	if (incorrectWord == "aret" && mutualChoice == "t")
+	{
+		points++;
+	}
+}
+void practisePresentContinuousTenseEx5(string mutualChoice)
+{
+	string sentence[10] = { "1. A: Where are you going?\n  B: I ___ going to school.",
+	"2. A: What are you doing?\n  B: I am ___ the guitar.","3. A: What are they doing?\n  B: ___ are playing football.",
+	"4. A: ___ we starting?\n  B: Yes, we are.","5. A: Why are you ___?\n  B: Because, I fell terrible.",
+	"6. A: Why ___ they skiing?\n  B: I have no idea.","7. A: Are you studying?\n  B: Yes, I ___.",
+	"8. A: I love reading detective stories.\n  B: Realy? At this moment I ___ reading detective story.",
+	"9. A: Is the girl studying ___?\n  B: Yes, she is.",
+	"10. A: Are you having a bath right now?\n  B: No, I __." };
+	string condition[1] = { "Enter The Correct Answer (Enter The Letter Only!): " };
+	system("cls");
+	printf("\t\t\t\t\t   Welcome to LET (Learn English Tenses)!");
+	printf("\n\n\nEx.5 Choose The Correct Answer.");
+	cout << "\n\n" << sentence[0];
+	printf("\n\n\ta) am  b) is");
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "a" || mutualChoice == "a)")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[1];
+	printf("\n\n\ta) playing  b) play");
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "a" || mutualChoice == "a)")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[2];
+	printf("\n\n\ta) We  b) They");
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "b" || mutualChoice == "b)")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[3];
+	printf("\n\n\ta) Are  b) Do");
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "a" || mutualChoice == "a)")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[4];
+	printf("\n\n\ta) crying  b) cry");
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "a" || mutualChoice == "a)")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[5];
+	printf("\n\n\ta) we  b) are");
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "b" || mutualChoice == "b)")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[6];
+	printf("\n\n\ta) do  b) does");
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "a" || mutualChoice == "a)")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[7];
+	printf("\n\n\ta) do  b) am");
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "b" || mutualChoice == "b)")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[8];
+	printf("\n\n\ta) now  b) general");
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "a" || mutualChoice == "a)")
+	{
+		points++;
+	}
+	cout << "\n\n" << sentence[9];
+	printf("\n\n\ta) doesn't b) don't");
+	cout << "\n\n" << condition[0];
+	getline(cin, mutualChoice);
+	if (mutualChoice == "b" || mutualChoice == "b)")
+	{
+		points++;
+	}
+}
 int main()
 {
 
